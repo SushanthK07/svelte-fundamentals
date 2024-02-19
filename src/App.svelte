b2abe255090e1f01087528e5a40f48c7912b133d
Sushanth Kummitha <sushanth.kummitha@needl.ai>
2024-02-20 00:58:44 +0530

    binding classes

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,44 @@
 <script>
 	const name = "Sushanth";
+	const age = 27;
 	const role = "<b>Software Engineer</b>";
 	const id = "sushanthk07";
 	const disabled = false;
+	const status = "success";
+	const married = false;
 </script>
 
 <main>
 	<!-- <h1 id={id}>Hello {name}!</h1> -->
 	<h1 {id}>Hello {name}!</h1>
 	<p>You are a {@html role}</p>
-	<button {disabled}>Bind</button>
+	<p class="underline">Your age: {age}</p>
+	<p class={status}>Status: {status}</p>
+	<!-- <p class={married ? 'married' : ''}>Married: {married}</p> -->
+	<!-- <p class:married={married}>Married: {married}</p> -->
+	<p class:married>Married: {married}</p>
+	<button {disabled}>Toggle</button>
 </main>
 
 <style>
+	/* static class */
+	.underline {
+		text-decoration: underline;
+	}
+
+	/* dynamic class */
+	.danger {
+		color: red;
+	}
+
+	.success {
+		color: green;
+	}
+
+	.married {
+		color: pink;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
