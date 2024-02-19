f902cf6928d7876b17ce50a889022ec1b553c529
Sushanth Kummitha <sushanth.kummitha@needl.ai>
2024-02-20 01:33:39 +0530

    counter

---
+++ src/Counter.svelte
@@ -0,0 +1,32 @@
+<script>
+    let count = 0;
+
+    const increment = (event, value = 1) => {
+        console.log(event);
+        count += value;
+    };
+
+    const decrement = (_, value = 1) => {
+        count -= value;
+    };
+</script>
+
+<main>
+    <h1>The count is {count}</h1>
+    <button on:click={increment}>
+        Click to increment
+    </button>
+    <button on:click={decrement}>
+        Click to decrement
+    </button>
+    <button on:click={() => count = 0}>
+        Click to reset
+    </button>
+    <br />
+    <button on:click={(event) => increment(event, 5)}>
+        Increase by 5
+    </button>
+    <button on:click={(event) => decrement(event, 5)}>
+        Decrease by 5
+    </button>
+</main>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import App from "./App.svelte";
 import ConditionalRendering from "./ConditionalRendering.svelte";
+import Counter from "./Counter.svelte";
 
-const app = new App({
+const app = new Counter({
   target: document.body,
 });
 
